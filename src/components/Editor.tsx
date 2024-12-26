5fad796c3707268e88e9d7d18f93be69973de420
Saketben10 <madmansaketkumar@gmail.com>
2024-12-27 00:01:15 +0530

    added uplaod image

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,6 @@
 import { PiTextAaBold } from "react-icons/pi";
 
-import { ImageIcon, Send, Smile } from "lucide-react";
+import { ImageIcon, Send, Smile, XIcon } from "lucide-react";
 
 import { Button } from "./ui/button";
 
@@ -44,6 +44,7 @@ const Editor = ({
 }: EditorPorps) => {
   const [Text, setText] = useState("");
   const [isToolBarVisisble, setisToolBarVisible] = useState(true);
+  const [image, setImage] = useState<File | null>(null);
 
   const containerRef = useRef<HTMLDivElement | null>(null);
   const disableRef = useRef(disabled);
@@ -51,6 +52,7 @@ const Editor = ({
   const placeholderRef = useRef(placeholder);
   const quilRef = useRef<Quill | null>(null);
   const defaultvalueRef = useRef(defaultValue);
+  const imageElementRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
@@ -141,11 +143,33 @@ const Editor = ({
 
   return (
     <div className="flex flex-col">
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageElementRef}
+        onChange={(event) => setImage(event.target.files![0])}
+        className="hidden"
+      />
       <div
         className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white
       "
       >
         <div ref={containerRef} className="h-full ql-custom" />
+        {!!image && (
+          <div className="p-2">
+            <div className="relative size-[62px] flex items-center justify-center  group/image  ">
+              <button
+                onClick={() => {
+                  setImage(null);
+                  imageElementRef.current!.value = "";
+                }}
+                className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+              >
+                <XIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        )}
         <div className="flex px-2 pb-2 z-[5]">
           <Hint
             label={isToolBarVisisble ? "Hide Formatting" : "show Formating"}
@@ -175,7 +199,7 @@ const Editor = ({
                 disabled={disabled}
                 variant={"ghost"}
                 size="iconsm"
-                onClick={() => {}}
+                onClick={() => imageElementRef.current?.click()}
               >
                 <ImageIcon className="size-4" />
               </Button>
